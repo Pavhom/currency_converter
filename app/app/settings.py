3a04c688d9bbaa2483d0b4546dc8cde380f85a80
Pavhom <104794074+Pavhom@users.noreply.github.com>
2022-11-04 03:55:10 +0200

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4gi=94qw%of-(n6p8*tun-n!8#or+ja46(!7zk810fs*vy-jon'
-EXCHANGERATE_API = 'd696260972222c725e5a1e45'
+SECRET_KEY = ''
+EXCHANGERATE_API = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
